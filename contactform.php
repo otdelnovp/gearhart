56155bda9ee79ede011813bee5d0ee1c1ba0e00d
Pavel <p4sh0k@gmail.com>
2016-10-13 10:37:38 +0300

    add

---
+++ contactform.php
@@ -0,0 +1,14 @@
+<?php
+
+$recepient = "DanielGearhart41@gmail.com";
+$sitename = "Gearhart Site";
+
+$firstname = trim($_POST["firstname"]);
+$lastname = trim($_POST["lastname"]);
+$email = trim($_POST["email"]);
+$service = trim($_POST["service"]);
+$text = trim($_POST["message"]);
+$message = "Name: $firstname $lastname \nEmail: $email \nService: $service \nMessage: $text";
+
+$pagetitle = "New client from \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
---
+++ js/common.js
@@ -0,0 +1,31 @@
+$(function() {
+
+	$('.header__menu-link, .header__menu__item_close a').click(function(){
+		$('.header__menu').toggleClass('show');
+		return false;
+	});
+
+	$('.gallery').magnificPopup({
+		delegate: '.preview__image a',
+		type: 'image',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1]
+		}
+	});
+
+	$("#contactform").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "contactform.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$("#contactform .success").show();
+			$("#contactform").trigger("reset");
+		});
+		return false;
+	});
+
+});
